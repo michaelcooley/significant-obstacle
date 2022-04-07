1fc53235bce299724d3cc5575f82e67de280b4e7
Michael Cooley <michael.cooley@daugherty.com>
2022-04-07 12:19:12 -0500

    implement npc pick up and drop

--- src/npcActions.js
+++ src/npcActions.js
@@ -13,9 +13,30 @@ export function npcActions(currentNPCLocation, npcData, locations, items) {
     location = locations[where];
   }
 
-  if (randoPickup < npcData.percentPickup) console.log('time for npc to pick up something');
-  if (randoDrop < npcData.percentDrop) console.log('time for npc to drop something');
+  if (randoPickup < npcData.percentPickup) {
+    let pickedUp = false;
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].location === currentNPCLocation.name) { //found item here, pick it up -- always pick up first item found
+        items[i].location = 'npc';
+        pickedUp = true;
+        console.log(`npc picked up ${items[i].name}`);
+        break;
+      }
+    }
+    if (!pickedUp) console.log('npc found nothing to pick up');
+  }
 
-  //or--if in same location as player--randomly say things
+  if (randoDrop < npcData.percentDrop) {
+    let dropped = false;
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].location === 'npc' && items[i].name !== npcData.retainItem) { //found item npc is carrying
+        items[i].location = currentNPCLocation.name;
+        dropped = true;
+        console.log(`npc dropped ${items[i].name} at ${currentNPCLocation.name}`);
+        break;
+      }
+    }
+    if (!dropped) console.log('npc found nothing to drop');
+  }
   return location;
 }
